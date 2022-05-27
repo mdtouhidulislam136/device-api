4e21eda8008d13ac12f84cbce54fac0c17a44971
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2022-05-27 21:24:50 +0300

    call added and location showing

--- app/src/main/java/com/example/deviceapi/MainActivity.java
+++ app/src/main/java/com/example/deviceapi/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.deviceapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleSetAlarmButtonClick(View view) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, "Wake up!")
+                .putExtra(AlarmClock.EXTRA_LENGTH, 15)
+                .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void handleCallButtonClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + "+358466805257"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    
+
+    public void handleLocation(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri geoLocation = null;
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
